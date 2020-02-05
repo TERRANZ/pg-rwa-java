6edce189a4c248433247760a08ee4bbe1aa88dbe
Vadim Korostelev <vadim.korostelev@aurea.com>
2020-02-05 16:28:05 +0400

    incr

---
+++ src/main/java/com/trilogy/peoplegraph/Main.java
@@ -0,0 +1,13 @@
+package com.trilogy.peoplegraph;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@SpringBootApplication(scanBasePackages = "com.trilogy.peoplegraph")
+@EnableWebMvc
+public class Main {
+
+    public static void main(String... args) {
+
+    }
+}
